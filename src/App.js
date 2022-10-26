7c9bc9b2a139384392e2d8f0ec40f377b5b02bc2
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-10-26 19:43:07 -0300

    finalizado requisito 3 e 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: 0,
+    cardAttr2: 0,
+    cardAttr3: 0,
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: false,
+    hasTrunfo: false,
+  };
+
+  handleValidation = (name) => {
+    if (name === 'cardTrunfo') {
+      this.setState({ cardTrunfo: !this.cardTrunfo });
+    }
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    if (name === 'cardTrunfo') {
+      this.handleValidation(name);
+    } else {
+      this.setState(({
+        [name]: value,
+      }));
+    }
+  };
+
   render() {
     return (
       <div>
         <h1>Tryunfo </h1>
-        <Form />
+        <Form onInputChange={ this.onInputChange } { ...this.state } />
+        <Card { ...this.state } />
       </div>
     );
   }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,15 +4,14 @@ import PropTypes from 'prop-types';
 class Form extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage,
-      // eslint-disable-next-line no-unused-vars
-      cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      cardRare, cardTrunfo, isSaveButtonDisabled,
       onInputChange, onSaveButtonClick } = this.props;
     return (
       <form>
         <label htmlFor="name">
           Nome da carta
           <input
-            id="name"
+            name="cardName"
             value={ cardName }
             onChange={ onInputChange }
             type="text"
@@ -22,6 +21,7 @@ class Form extends React.Component {
         <label htmlFor="descriçao">
           Descrição
           <textarea
+            name="cardDescription"
             value={ cardDescription }
             onChange={ onInputChange }
             data-testid="description-input"
@@ -31,6 +31,7 @@ class Form extends React.Component {
         <label htmlFor="attr1">
           Velocidade
           <input
+            name="cardAttr1"
             value={ cardAttr1 }
             onChange={ onInputChange }
             type="number"
@@ -40,6 +41,7 @@ class Form extends React.Component {
         <label htmlFor="attr2">
           Arrancada
           <input
+            name="cardAttr2"
             value={ cardAttr2 }
             onChange={ onInputChange }
             type="number"
@@ -49,6 +51,7 @@ class Form extends React.Component {
         <label htmlFor="attr3">
           Estabilidade
           <input
+            name="cardAttr3"
             value={ cardAttr3 }
             onChange={ onInputChange }
             type="number"
@@ -58,6 +61,7 @@ class Form extends React.Component {
         <label htmlFor="img">
           Imagem
           <input
+            name="cardImage"
             value={ cardImage }
             onChange={ onInputChange }
             type="text"
@@ -65,9 +69,9 @@ class Form extends React.Component {
           />
         </label>
         <select
+          name="cardRare"
           value={ cardRare }
           onChange={ onInputChange }
-          name="radidade"
           data-testid="rare-input"
         >
           <option>normal</option>
@@ -77,6 +81,8 @@ class Form extends React.Component {
         <label htmlFor="check">
           Super Trunfo
           <input
+            id="check"
+            name="cardTrunfo"
             checked={ cardTrunfo }
             onChange={ onInputChange }
             type="checkbox"
@@ -104,7 +110,6 @@ Form.propTypes = {
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
-  hasTrunfo: PropTypes.bool.isRequired,
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
